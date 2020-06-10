04f04ff2282387f5d74ccc65bf4d10f876d46741
setieLouis <adjams@live.it>
2020-06-10 08:42:23 +0200

    [DELETE]: delete mocked static data base data

--- components/database/Paperbase.js
+++ components/database/Paperbase.js
@@ -127,7 +127,6 @@ export function findBlockByid(id) {
 }
 
 export function findReceiptByShopId(shop_id) {
-  console.log(shop_id)
   return findAllReceipt().filtered('shop = "' + shop_id.toString() + '"');
 }
 
@@ -278,7 +277,7 @@ function blockModel(content, info = blockId()) {
   };
 }
 
-function receiptModel(content, shop) {
+export function receiptModel(content, shop) {
   return {
     id: receiptId(),
     shop: shop,
@@ -286,7 +285,7 @@ function receiptModel(content, shop) {
   };
 }
 
-function shopModel(id, name, address) {
+export function shopModel(id, name, address) {
   return {
     id: id,
     name: name,
@@ -308,148 +307,3 @@ export function toArray(collect) {
   }
   return list;
 }
-
-const content2 =
-  '                {*}*ESSELUNGA S.P.A*{*}             ' +
-  '\n            DOCUMENTO COMMERCIALE            ' +
-  '\n' +
-  '\n**********************************************' +
-  '\n*****       RECEVUTA DI PAGAMENTO        *****' +
-  '\nEsselunga via Famagosta\nPrepagate Virtuali' +
-  '\nS/E-CE 1163' +
-  '\nCASSA: 006 ID 00116306' +
-  '\nOPER: 27214 STAN 003452' +
-  '\nC 721973******4850 keyed' +
-  '\nCOD.AUT. 367506' +
-  '\nRESIDUO: 0,00\nACQ.ID 00000000029' +
-  '\n' +
-  '\n' +
-  '{m*}TOTALE                    3,55{m*}' +
-  '\n' +
-  '\nTRANSAZIONE AUTORIZZATTA' +
-  '\n*****      {RECEVUTA DI PAGAMENTO}       *****' +
-  '\n**********************************************' +
-  '\n' +
-  '\n' +
-  '{q}ciao mama come va{q}' +
-  '\n' +
-  '\n            DOCUMENTO COMMERCIALE            ';
-
-saveReceipt({
-  shop: shopModel('0', 'serafino', 'via Settembre'),
-  receipt: receiptModel(content2, '0'),
-});
-saveReceipt({
-  shop: shopModel('0', 'serafino', 'via Settembre'),
-  receipt: receiptModel(content2, '0'),
-});
-saveReceipt({
-  shop: shopModel('0', 'serafino', 'via Settembre'),
-  receipt: receiptModel(content2, '0'),
-});
-
-saveReceipt({
-  shop: shopModel('1', 'Pizza Per Tutti', 'Pzza 25 novembre'),
-  receipt: receiptModel(content2, '1'),
-});
-saveReceipt({
-  shop: shopModel('1', 'Pizza Per Tutti', 'Pzza 25 novembre'),
-  receipt: receiptModel(content2, '1'),
-});
-saveReceipt({
-  shop: shopModel('1', 'Pizza Per Tutti', 'Pzza 25 novembre'),
-  receipt: receiptModel(content2, '1'),
-});
-
-saveReceipt({
-  shop: shopModel('2', 'Grande Mercato', 'Pzza 25 novembre'),
-  receipt: receiptModel(content2, '2'),
-});
-saveReceipt({
-  shop: shopModel('2', 'Grande Mercato', 'Pzza 25 novembre'),
-  receipt: receiptModel(content2, '2'),
-});
-saveReceipt({
-  shop: shopModel('2', 'Grande Mercato', 'Pzza 25 novembre'),
-  receipt: receiptModel(content2, '2'),
-});
-
-saveReceipt({
-  shop: shopModel('3', 'Piccolo Mercato', 'via della disperazione 89'),
-  receipt: receiptModel(content2, '3'),
-});
-saveReceipt({
-  shop: shopModel('3', 'Piccolo Mercato', 'via della disperazione 89'),
-  receipt: receiptModel(content2, '3'),
-});
-saveReceipt({
-  shop: shopModel('3', 'Piccolo Mercato', 'via della disperazione 89'),
-  receipt: receiptModel(content2, '3'),
-});
-
-saveReceipt({
-  shop: shopModel('4', 'Jolie', 'ia della verdi 89'),
-  receipt: receiptModel(content2, '4'),
-});
-saveReceipt({
-  shop: shopModel('4', 'Jolie', 'ia della verdi 89'),
-  receipt: receiptModel(content2, '4'),
-});
-saveReceipt({
-  shop: shopModel('4', 'Jolie', 'via della verdi 89'),
-  receipt: receiptModel(content2, '4'),
-});
-
-saveReceipt({
-  shop: shopModel('5', 'Petite fille', 'Piazza Garilbadi Milano verdi 89'),
-  receipt: receiptModel(content2, '5'),
-});
-saveReceipt({
-  shop: shopModel('5', 'Petite fille', 'Piazza Garilbadi Milano verdi 89'),
-  receipt: receiptModel(content2, '5'),
-});
-saveReceipt({
-  shop: shopModel('5', 'Petite fille', 'Piazza Garilbadi Milano verdi 89'),
-  receipt: receiptModel(content2, '5'),
-});
-
-saveReceipt({
-  shop: shopModel('6', 'Al lago', 'via Como Milano verdi 89'),
-  receipt: receiptModel(content2, '6'),
-});
-saveReceipt({
-  shop: shopModel('6', 'Al lago', 'via Como Milano verdi 89'),
-  receipt: receiptModel(content2, '6'),
-});
-saveReceipt({
-  shop: shopModel('6', 'Al lago', 'via Como Milano verdi 89'),
-  receipt: receiptModel(content2, '6'),
-});
-
-saveReceipt({
-  shop: shopModel('7', 'Da Yemi', 'via della MyGosh 89'),
-  receipt: receiptModel(content2, '7'),
-});
-saveReceipt({
-  shop: shopModel('7', 'Da Yemi', 'via della MyGosh 89'),
-  receipt: receiptModel(content2, '7'),
-});
-saveReceipt({
-  shop: shopModel('7', 'Da Yemi', 'via della MyGosh 89'),
-  receipt: receiptModel(content2, '7'),
-});
-
-saveReceipt({
-  shop: shopModel('8', 'Mare alto', 'via Rimino Milano verdi 89'),
-  receipt: receiptModel(content2, '8'),
-});
-
-saveReceipt({
-  shop: shopModel('8', 'Mare alto', 'via Rimino Milano verdi 89'),
-  receipt: receiptModel(content2, '8'),
-});
-
-saveReceipt({
-  shop: shopModel('8', 'Mare alto', 'via Rimino Milano verdi 89'),
-  receipt: receiptModel(content2, '8'),
-});
