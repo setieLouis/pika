36d373893912691867ba20380ec8599cfe172a82
setieLouis <adjams@live.it>
2020-06-10 21:29:03 +0200

    [ADDING]: check for undestand if can  put database on redux box this view

--- components/Navigation/Navigator.js
+++ components/Navigation/Navigator.js
@@ -4,9 +4,10 @@ import ShopLister from '../Shop/ShopLister';
 import {Animated, Easing, Text, TextInput, View} from 'react-native';
 
 import ReceiptLister from '../Receipt/ReceiptLister';
+import {connect} from 'react-redux';
 const Stack = createStackNavigator();
 
-export default class Navigator extends React.Component {
+class Navigator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -30,3 +31,9 @@ export default class Navigator extends React.Component {
     );
   }
 }
+
+const mapStateToProps = state => {
+    console.log("sono passato dal navigatore ")
+    return {};
+};
+export default connect(mapStateToProps)(Navigator);
