7fbd56106ba96a87cc038302951117da1154ad6b
setieLouis <adjams@live.it>
2020-06-10 17:40:34 +0200

    [REFACTORING]: delete unused states

--- components/store/reducers/routeReducer.js
+++ components/store/reducers/routeReducer.js
@@ -1,46 +1,28 @@
 const initialState = {
-  paper: {
-    tag: {
-      tags: [],
-      updateTags: [],
-      deleteTags: [],
-    },
-    info: {
-      infos: [],
-      deleteInfos: [],
-    },
-  },
   shop: {
     receiveNew: true,
     all: [],
     delete: [],
   },
-
   receipt: {
     all: [],
     delete: [],
   },
-
   new: [],
 };
 
 function tagsHandler(state = initialState, action) {
-  let nextstate = state;
-  let list;
-  const stateTag = state.paper.tag;
-  const stateInfo = state.paper.info;
-  const paper = state.paper;
-
+  let nextstate;
+  const shop = state.shop;
   switch (action.type) {
     case 'GET_ALL_DATABASE':
-      if (state.shop.all.length > 0) {
+      if (shop.all.length > 0) {
         return state;
       }
-
       nextstate = {
         ...state,
         shop: {
-          ...state.shop,
+          ...shop,
           receiveNew: false,
           all: [...action.value.shops],
         },
@@ -51,13 +33,13 @@ function tagsHandler(state = initialState, action) {
       };
       return nextstate;
     case 'DELETE_SHOP':
-      const tmpAll = state.shop.all.filter(shop => shop.id !== action.value.id);
+      const tmpAll = shop.all.filter(shop => shop.id !== action.value.id);
       nextstate = {
         ...state,
         shop: {
-          ...state.shop,
+          ...shop,
           all: [...tmpAll],
-          delete: [...state.shop.delete, action.value],
+          delete: [...shop.delete, action.value],
         },
       };
       return nextstate;
@@ -66,9 +48,9 @@ function tagsHandler(state = initialState, action) {
       nextstate = {
         ...state,
         shop: {
-          ...state.shop,
+          ...shop,
           //receiveNew: true,
-          all: [action.value, ...state.shop.all],
+          all: [action.value, ...shop.all],
         },
         new: [...state.new, action.value],
         //TODO ADD RECEIPT
@@ -79,12 +61,11 @@ function tagsHandler(state = initialState, action) {
       nextstate = {
         ...state,
         shop: {
-          ...state.shop,
+          ...shop,
           delete: [],
         },
         new: [],
       };
-
       return nextstate;
     }
 
@@ -97,89 +78,10 @@ function tagsHandler(state = initialState, action) {
         },
       };
       return nextstate;
-    case 'ADD_NEW_TAGS':
-      nextstate = {
-        ...state,
-        paper: {
-          ...paper,
-          tag: {
-            ...stateTag,
-            tags: [...stateTag.tags, action.value],
-          },
-        },
-      };
-      break;
-    case 'UPDATE_TAG':
-      list = stateTag.tags.filter(tag => tag.id !== action.value.id);
-      nextstate = {
-        ...state,
-        paper: {
-          ...paper,
-          tag: {
-            ...stateTag,
-            tags: [...list, action.value],
-            updateTags: [...stateTag.updateTags, action.value],
-          },
-        },
-      };
-      break;
 
-    case 'ADD_TAG_LIST':
-      nextstate = {
-        ...state,
-        paper: {
-          ...paper,
-          tag: {
-            ...stateTag,
-            tags: [...action.value],
-          },
-        },
-      };
-      break;
-    case 'DELETE_TAG':
-      list = stateTag.tags.filter(tag => tag.id !== action.value.id);
-      nextstate = {
-        ...state,
-        paper: {
-          ...paper,
-          tag: {
-            ...stateTag,
-            tags: [...list],
-            deleteTags: [...stateTag.deleteTags, action.value],
-          },
-        },
-      };
-      break;
-    case 'ADD_INFO_LIST':
-      nextstate = {
-        ...state,
-        paper: {
-          ...paper,
-          info: {
-            ...stateInfo,
-            infos: [...action.value],
-          },
-        },
-      };
-      break;
-    case 'DELETE_INFO':
-      list = stateTag.tags.filter(tag => tag.id !== action.value.id);
-      nextstate = {
-        ...state,
-        paper: {
-          ...paper,
-          info: {
-            ...stateInfo,
-            infos: [...list],
-          },
-        },
-      };
-      break;
     default:
       return state;
   }
-  return nextstate;
-  //return nextState || state
 }
 
 export default tagsHandler;
